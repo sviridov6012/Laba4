c1b5d74c0c2e4fc8292e93fd486822b3754d4f6a
sviridov6012 <160075897+sviridov6012@users.noreply.github.com>
2024-11-14 01:09:41 +0300

    Update Task1.py

--- Лаба 4/Task1/Task1.py
+++ Лаба 4/Task1/Task1.py
@@ -8,7 +8,7 @@ def task() -> float:
     sum_products = 0
     for item in data:
         sum_products += item["score"]
-        item["weight"]
+        var = item["weight"]
 
     return round(sum_products, 3)
 
